b2103250447bca933e9c1ba7c2b971223cc0ee8f
LeWiz24 <93799604+LeWiz24@users.noreply.github.com>
2024-02-05 10:55:53 -0500

    Add files via upload

---
+++ Fundamentals2.playground/Contents.swift
@@ -0,0 +1,46 @@
+//  Exercise 1: Declaring and Unwrapping Optionals
+//  Declare an optional string variable named nickname that initially has no
+//  value. Then assign a value and safely unwrap it with an if let statement to
+//  print it.
+
+// Write your code below:
+
+
+
+
+
+// Exercise #2: Create a class `Person` with an optional property `nickname`. Then instantiate a `Person` and use optional chaining to print the nickname if it exists.
+
+// Write your code below:
+
+
+
+
+
+// Exercise #3: Implicitly Unwrapped Optionals
+// An implicitly unwrapped optional is a type of optional that you assume will always have a value after it's first set.
+// Below, define a Dog class with a name property and an implicitly unwrapped optional property nickname.
+// Then, instantiate a Dog object with and without a nickname, and demonstrate how you might access the nickname.
+// In your comments, discuss why using an implicitly unwrapped optional could be beneficial and what risks it might pose.
+
+// Write your code below:
+
+
+
+
+
+
+// Exercise #4: Type Casting w/ Optionals
+//  Create an array of different types (Int, String, Double) as [Any], then write a function that iterates over the array and prints the type and value of each element using optional casting.
+
+// Write your code below:
+
+
+
+// Exercise #5: Advanced Optional Handling with guard
+// Create a Cat class that includes a method that uses 'guard let' to safely unwrap the nickname
+
+// Write your code below:
+
+
+}
